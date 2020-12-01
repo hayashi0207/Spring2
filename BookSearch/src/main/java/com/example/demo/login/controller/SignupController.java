38ffdf73913f4ab4cc756df56e48c8a5a33879bb
Hayashi Ryunosuke <hayashi.ryunosuke.mdk@outlook.jp>
2020-12-01 14:44:55 +0900

    バリデーションに優先順位をつける。

--- BookSearch/src/main/java/com/example/demo/login/controller/SignupController.java
+++ BookSearch/src/main/java/com/example/demo/login/controller/SignupController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.GroupOrder;
 import com.example.demo.login.domain.model.SignupForm;
 
 @Controller
@@ -19,7 +20,7 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute @Validated SignupForm form,BindingResult bindingResult, Model model) {
+	public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form,BindingResult bindingResult, Model model) {
 		if(bindingResult.hasErrors()) {
 			return getSignUp(form,model);
 		}
