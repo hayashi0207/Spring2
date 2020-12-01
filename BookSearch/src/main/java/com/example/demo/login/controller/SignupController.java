3d0994cf449a9470404effcaeee71b41560aff5f
Hayashi Ryunosuke <hayashi.ryunosuke.mdk@outlook.jp>
2020-12-01 10:29:59 +0900

    バリデーション実装

--- BookSearch/src/main/java/com/example/demo/login/controller/SignupController.java
+++ BookSearch/src/main/java/com/example/demo/login/controller/SignupController.java
@@ -3,6 +3,7 @@ package com.example.demo.login.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute SignupForm form,BindingResult bindingResult, Model model) {
+	public String postSignUp(@ModelAttribute @Validated SignupForm form,BindingResult bindingResult, Model model) {
 		if(bindingResult.hasErrors()) {
 			return getSignUp(form,model);
 		}
