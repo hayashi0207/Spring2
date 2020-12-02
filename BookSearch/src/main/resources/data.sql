2ffc0da0307ec8f4c258ac9d76253dfc849b21b1
Hayashi Ryunosuke <hayashi.ryunosuke.mdk@outlook.jp>
2020-12-02 10:32:59 +0900

    JdbcTemplateの骨組み作成

--- BookSearch/src/main/resources/data.sql
+++ BookSearch/src/main/resources/data.sql
@@ -4,3 +4,7 @@ VALUES
     (1, 'Spring解体新書', 2019),
     (2, 'Spring Security解体新書', 2020),
     (3, 'Java デザインパターン', 2017);
+
+INSERT INTO m_user
+    (user_id,password,user_name,birthday,age,role)
+VALUES('hayashi@xxx.co.jp', 'password', '林太郎', '1990-01-01', 28, 'ROLE_ADMIN');
--- BookSearch/src/main/resources/schema.sql
+++ BookSearch/src/main/resources/schema.sql
@@ -6,3 +6,18 @@ IF NOT EXISTS book
 (50),
     book_publication_year INT
 );
+
+CREATE TABLE
+IF NOT EXISTS m_user
+(
+    user_id VARCHAR
+(50) PRIMARY KEY,
+    password VARCHAR
+(100),
+    user_name VARCHAR
+(50),
+    birthday DATE,
+    age INT,
+    role VARCHAR
+(50)
+);
