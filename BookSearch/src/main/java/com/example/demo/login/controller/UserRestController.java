05bef7e8f2986b3aba2158f225bcd77fe614cd42
Hayashi Ryunosuke <hayashi.ryunosuke.mdk@outlook.jp>
2020-12-11 08:03:47 +0900

    mybatis 実装

--- BookSearch/src/main/java/com/example/demo/login/controller/UserRestController.java
+++ BookSearch/src/main/java/com/example/demo/login/controller/UserRestController.java
@@ -1,5 +1,30 @@
 package com.example.demo.login.controller;
 
-public class UserRestController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.service.RestService;
+
+@RestController
+public class UserRestController {
+	
+	@Autowired
+	@Qualifier("RestServiceMybatisImpl")
+	RestService service;
+	
+	@GetMapping("/rest/get")
+	public List<User> getUserMany(){
+		return service.selectMany();
+	}
+	
+	@GetMapping("/rest/get/{id:.+}")
+	public User getUserOne(@PathVariable("id")String userId) {
+		return service.selectOne(userId);
+	}
 }
--- BookSearch/src/main/java/com/example/demo/login/domain/service/mybatis/RestServiceMybatisImpl.java
+++ BookSearch/src/main/java/com/example/demo/login/domain/service/mybatis/RestServiceMybatisImpl.java
@@ -1,45 +1,42 @@
-package com.example.demo.login.domain.service.jdbc;
+package com.example.demo.login.domain.service.mybatis;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.login.domain.model.User;
-import com.example.demo.login.domain.repository.IUserDao;
+import com.example.demo.login.domain.repository.mybatis.UserMapper;
 import com.example.demo.login.domain.service.RestService;
 
 @Transactional
-@Service
-public class RestServiceJdbcImpl implements RestService{
+@Service("RestServiceMybatiImpl")
+public class RestServiceMybatisImpl implements RestService{
 	@Autowired
-	@Qualifier("UserDaoJdbcImpl")
-	IUserDao dao;
+	UserMapper userMapper;
 	
 	@Override
 	public boolean insert(User user) {
-		return false;
+		return userMapper.insert(user);
 	}
 	
 	@Override
 	public User selectOne(String userId) {
-		return dao.selectOne(userId);
+		return userMapper.selectOne(userId);
 	}
 	
 	@Override
 	public List<User> selectMany(){
-		return dao.selectMany();
+		return userMapper.selectMany();
 	}
 	
 	@Override
 	public boolean update(User user) {
-		return false;
+		return userMapper.updateOne(user);
 	}
-	
 	@Override
 	public boolean delete(String userId) {
-		return false;
+		return userMapper.deleteOne(userId);
 	}
 }
